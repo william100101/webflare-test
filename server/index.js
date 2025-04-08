32817c9f34bafdcf790523446fbc6fbad71123cd
william100101 <wcc007@bucknell.edu>
2025-04-08 16:31:31 -0400

    pushing up stuff

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const app = express()
+const app = express();
+const cos = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"] // update if you change client port
+}
+
+app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
-
+ 
     
 
+})
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
 })
